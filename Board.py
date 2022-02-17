bd7125dd927a286695f384fe70e7319225c7da6b
Vasapg <99091199+Vasapg@users.noreply.github.com>
2022-02-17 16:58:39 +0100

    Fixed Coord on init(), add show for any turn

--- Board.py
+++ Board.py
@@ -8,34 +8,49 @@ class Board:
             self.board[i] = [None] * 8
         x = 0
         y = 0
-        self.board[x][y] = Rook(y, x, "W")
-        self.board[x][7 - y] = Rook(y, x, "W")
-        self.board[7][y] = Rook(y, x, "B")
-        self.board[7][7 - y] = Rook(y, x, "B")
+        self.board[x][y] = Rook(y, x, True)
+        self.board[x][7 - y] = Rook(y, x, True)
+        self.board[7][y] = Rook(y, x, False)
+        self.board[7][7 - y] = Rook(y, x, False)
         y = y + 1
-        self.board[x][y] = Knight(y, x, "W")
-        self.board[x][7 - y] = Knight(y, x, "W")
-        self.board[7][y] = Knight(y, x, "B")
-        self.board[7][7 - y] = Knight(y, x, "B")
+        self.board[x][y] = Knight(y, x, True)
+        self.board[x][7 - y] = Knight(y, x, True)
+        self.board[7][y] = Knight(y, x, False)
+        self.board[7][7 - y] = Knight(y, x, False)
         y = y + 1
-        self.board[x][y] = Bishop(y, x, "W")
-        self.board[x][7 - y] = Bishop(y, x, "W")
-        self.board[7][y] = Bishop(y, x, "B")
-        self.board[7][7 - y] = Bishop(y, x, "B")
+        self.board[x][y] = Bishop(y, x, True)
+        self.board[x][7 - y] = Bishop(y, x, True)
+        self.board[7][y] = Bishop(y, x, False)
+        self.board[7][7 - y] = Bishop(y, x, False)
         y = y + 1
-        self.board[x][y] = Queen(y, x, "W")
-        self.board[7][3] = Queen(y, x, "B")
+        self.board[x][y] = Queen(y, x, True)
+        self.board[7][3] = Queen(y, x, False)
         y = y + 1
-        self.board[x][y] = King(y, x, "W")
-        self.board[7][4] = King(y, x, "B")
+        self.board[x][y] = King(y, x, True)
+        self.board[7][4] = King(y, x, False)
         y = 0
         x = 1
         while y != 8:
-            self.board[x][y] = Pawn(y, x, "W")
-            self.board[7 - 1][y] = Pawn(y, x, "B")
+            self.board[x][y] = Pawn(y, x, True)
+            self.board[7 - 1][y] = Pawn(y, x, False)
             y = y + 1
+        self.turn = True;      #if turn==True, White plays, if turn==False, Black plays
 
-    def show(self):
+    def showW(self):
+        print("")
+        n=8
+        for i in reversed(self.board):
+            print(n, end=" ")
+            for j in reversed(i):
+                if j is None:
+                    print("[ ]", end=" ")
+                else:
+                    j.name()
+            print("")
+            n=n-1
+        print("   A   B   C   D   E   F   G   H ")
+
+    def showB(self):
         print("")
         n=1
         for i in self.board:
@@ -47,8 +62,7 @@ class Board:
                     j.name()
             print("")
             n=n+1
-        print("   A   B   C   D   E   F   G   H ")
-        print("")
+        print("   H   G   F   E   D   C   B   A ")
 
     def play(self, OrigX, OrigY, DestX, DestY):
         if self.board[OrigY][OrigX] is None:
@@ -58,17 +72,21 @@ class Board:
             PieceAux2 = self.board[DestY][DestX]
             if PieceAux.color == PieceAux2.color:
                 return print("!>>invalid move: there is a piece of your team in the destiny coordinates given")
-        if not PieceAux.canMove(DestY ,DestX):
+        if not PieceAux.canMove(DestY, DestX):
             return print("!>>invalid move: move not allowed for piece ", {PieceAux.getType()})
 
         self.board[DestY][DestX] = PieceAux
         self.board[OrigY][OrigX] = None
         PieceAux.setPos(DestY, DestX)
+        self.turn = not self.turn
 
 
 
 
-# tablero = Board()
-# tablero.show()
-# tablero.play(1,1 ,2 ,2 )
-# tablero.show()
+tablero = Board()
+tablero.showW()
+tablero.play(1, 0, 2, 3 )
+tablero.showB()
+tablero.play(6, 7, 5, 5)
+tablero.showW()
+tablero.showB()
