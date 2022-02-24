5cb9648603280e9de2dfc9dcdf1519461844d535
Vasapg <victoralejandro.sanz@alumnos.upm.es>
2022-02-24 12:56:33 +0100

    Added a missing self in terminal, Fixing play and problems with arrays

--- Board.py
+++ Board.py
@@ -13,33 +13,33 @@ class Board:
             self.board[i] = [None] * 8
         x = 0  # Declare x a y, int values, that makes an easier declaration of the piecec
         y = 0  # on the 8x8 matrix
-        self.board[x][y] = Rook(y, x, True)  # Initialization of every piece in their ->
-        self.board[x][7 - y] = Rook(y, x, True)  # -> respective position at the start of the game
-        self.board[7][y] = Rook(y, x, False)
-        self.board[7][7 - y] = Rook(y, x, False)
+        self.board[x][y] = Rook(y, x, False)  # Initialization of every piece in their ->
+        self.board[x][7 - y] = Rook(y, x, False)  # -> respective position at the start of the game
+        self.board[7][y] = Rook(y, x, True)
+        self.board[7][7 - y] = Rook(y, x, True)
         y = y + 1
-        self.board[x][y] = Knight(y, x, True)
-        self.board[x][7 - y] = Knight(y, x, True)
-        self.board[7][y] = Knight(y, x, False)
-        self.board[7][7 - y] = Knight(y, x, False)
+        self.board[x][y] = Knight(y, x, False)
+        self.board[x][7 - y] = Knight(y, x, False)
+        self.board[7][y] = Knight(y, x, True)
+        self.board[7][7 - y] = Knight(y, x, True)
         y = y + 1
-        self.board[x][y] = Bishop(y, x, True)
-        self.board[x][7 - y] = Bishop(y, x, True)
-        self.board[7][y] = Bishop(y, x, False)
-        self.board[7][7 - y] = Bishop(y, x, False)
+        self.board[x][y] = Bishop(y, x, False)
+        self.board[x][7 - y] = Bishop(y, x, False)
+        self.board[7][y] = Bishop(y, x, True)
+        self.board[7][7 - y] = Bishop(y, x, True)
         y = y + 1
-        self.board[x][y] = Queen(y, x, True)
-        self.board[7][3] = Queen(y, x, False)
+        self.board[x][y] = Queen(y, x, False)
+        self.board[7][3] = Queen(y, x, True)
         y = y + 1
-        self.board[x][y] = King(y, x, True)
-        self.board[7][4] = King(y, x, False)
+        self.board[x][y] = King(y, x, False)
+        self.board[7][4] = King(y, x, True)
         y = 0
         x = 1
         # Loop to make easier the initialization of 16 Pawns from both teams, setting their
         # y coordinate still and adding 1 to x in every iteration
         while y != 8:
-            self.board[x][y] = Pawn(y, x, True)
-            self.board[7 - 1][y] = Pawn(y, x, False)
+            self.board[x][y] = Pawn(y, x, False)
+            self.board[7 - 1][y] = Pawn(y, x, True)
             y = y + 1
 
     def setCoord(self, Coord, Piece):
@@ -51,9 +51,9 @@ class Board:
     def getTurn(self):
         return self.turn
 
-    def showW(self):  # Prints Board in the point of view of the White team
+    def showB(self):  # Prints Board in the point of view of the White team
         print("")
-        n = 8
+        n = 1
         for i in reversed(self.board):
             print(n, end=" ")
             for j in reversed(i):
@@ -62,12 +62,12 @@ class Board:
                 else:
                     j.name()
             print("")
-            n = n - 1
-        print("   A   B   C   D   E   F   G   H ")
+            n = n + 1
+        print("   H   G   F   E   D   C   B   A ")
 
-    def showB(self):  # Prints Board in the point of view of the Black team
+    def showW(self):  # Prints Board in the point of view of the Black team
         print("")
-        n = 1
+        n = 8
         for i in self.board:
             print(n, end=" ")
             for j in i:
@@ -76,8 +76,8 @@ class Board:
                 else:
                     j.name()
             print("")
-            n = n + 1
-        print("   H   G   F   E   D   C   B   A ")
+            n = n - 1
+        print("   A   B   C   D   E   F   G   H ")
 
     def play(self, coordinate_orig, coordinate_dest):
         # 0º Condition: there is a piece in the Origin Coordinates
@@ -90,21 +90,27 @@ class Board:
             return print("!>>invalid move: The piece selected it's not from your team")
 
         # 2º Condition: If there is a piece in the Destiny, its an enemy
-        if self.getOnCoord(coordinate_dest) is not None:
-            PieceAux2 = self.getOnCoord(coordinate_dest)
+        PieceAux2 = self.getOnCoord(coordinate_dest)
+        if PieceAux2 is not None:
             if PieceAux.getColor() == PieceAux2.color:
                 return print("!>>invalid move: there is a piece of your team in the destiny coordinates given")
 
         # 3º Condition: The move is allowed for the piece selected (Also treating pawn's special diagonal move)
-        if PieceAux is Pawn and PieceAux2 is not None:
-            PieceAux.canMove(2, 3, True)
-        elif not PieceAux.canMove(2, 2):
+        if type(PieceAux) is Pawn and PieceAux2 is not None:
+            if PieceAux.canMove(coordinate_dest.getX(), coordinate_dest.getY(), True) is []:
+                return print("!>>invalid move: move not allowed for piece ", {PieceAux.getType()})
+        elif type(PieceAux) is Pawn and PieceAux2 is None:
+            if PieceAux.canMove(coordinate_dest.getX(), coordinate_dest.getY(), False) is []:
+                return print("!>>invalid move: move not allowed for piece ", {PieceAux.getType()})
+        elif PieceAux.canMove(coordinate_dest.getX(), coordinate_dest.getY()) is []:
             return print("!>>invalid move: move not allowed for piece ", {PieceAux.getType()})
+        else:
+            positions = PieceAux.canMove(coordinate_dest.getX(), coordinate_dest.getY())
 
         # 4º Condition: There is not a Piece blocking your move (Not including Knight)
         # if PieceAux is not Knight:
         #     valid = True
-        #     for j in i:
+        #     for j in positions:
         #         if self.getOnCoord(j) is not None:
         #             valid = False
         #             break
@@ -128,12 +134,15 @@ class Board:
 
 # Pruebas:
 
-tablero = Board()
-tablero.showW()
-tablero.play(Coordinate(4, 0), Coordinate(4, 3))
-tablero.showW()
-tablero.play(Coordinate(4, 6), Coordinate(4, 7))
-tablero.showW()
+# Prueba = Pawn(0,0,True)
+# print(type(Prueba) is Pawn)
+
+# tablero = Board()
+# tablero.showW()
+# tablero.play(Coordinate(6, 7), Coordinate(5, 5))
+# tablero.showB()
+# tablero.play(Coordinate(4, 6), Coordinate(4, 7))
+# tablero.showW()
 
 # ANTIGUO PLAY, POR SI LO NECESITO DESPUES
 # def play(self, OrigX, OrigY, DestX, DestY):
