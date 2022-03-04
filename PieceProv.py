f8d3510547c88b7554d695a0afa4cbe0f3ebf4e1
Vasapg <99091199+Vasapg@users.noreply.github.com>
2022-03-04 21:25:44 +0100

    Added method getX and getY on class Piece

--- PieceProv.py
+++ PieceProv.py
@@ -4,17 +4,22 @@ from coordinates import Coordinate
 class Piece:
 
     def __init__(self, posx, posy, color):
-        self.coordinate=Coordinate(0,0)
-        self.coordinate.setX(posx)
-        self.coordinate.setY(posy)
+        self.posx = posx
+        self.posy = posy
         self.color = color
 
     def getColor(self):
         return self.color
 
     def setPos(self, posx, posy):
-        self.coordinate.setY(posy)
-        self.coordinate.setX(posx)
+        self.posy = posy
+        self.posx = posx
+
+    def getX(self):
+        return self.posx
+
+    def getY(self):
+        return self.posy
 
     def __str__(self):
         return f"[ ]"
@@ -54,21 +59,21 @@ class Pawn(Piece):
         if not pieceAt:
             # Check if this pawn is black or white
             if not self.color: # If its black the movement is inverted
-                if self.firstmove and posy - self.coordinate.getY() == 2:
-                    return [Coordinate(posx, self.coordinate.getY() + 1), Coordinate(posx, self.coordinate.getY()+2)]
-                elif posy - self.coordinate.getY() == 1:
+                if self.firstmove and posy - self.posy == 2:
+                    return [Coordinate(posx, self.posy + 1), Coordinate(posx, self.posy+2)]
+                elif posy - self.posy == 1:
                     return [Coordinate(posx, posy)]
             if self.color: # If its white the movement is normal
-                if self.firstmove and self.coordinate.getY() - posy == 2:
-                    return [Coordinate(posx, self.coordinate.getY() - 1), Coordinate(posx, self.coordinate.getY()-2)]
-                elif self.coordinate.getY() - posy == 1:
+                if self.firstmove and self.posy - posy == 2:
+                    return [Coordinate(posx, self.posy - 1), Coordinate(posx, self.posy-2)]
+                elif self.posy - posy == 1:
                     return [Coordinate(posx, posy)]
         else: # If the movement is to capture a piece we check if the movement is valid
             if not self.color:
-                if ((self.coordinate.getY() - posy) == 1) and (-1 <= (posx - self.coordinate.getX()) <= 1):
+                if ((self.posy - posy) == 1) and (-1 <= (posx - self.posx) <= 1):
                     return [Coordinate(posx, posy)]
             if self.color:
-                if ((posy - self.coordinate.getY()) == 1) and (-1 <= (posx - self.coordinate.getX()) <= 1):
+                if ((posy - self.posy) == 1) and (-1 <= (posx - self.posx) <= 1):
                     return [Coordinate(posx, posy)]
         return positions
 
@@ -96,26 +101,26 @@ class Rook(Piece):
         position = []
         auxcoord = 0;
         # We have to check if the movement is vertical or horizontal
-        if self.coordinate.getY() - posy == 0: # If the Y vertex is the same, the movement is horizontal
-            if self.coordinate.getX() - posx > 0:
+        if self.posy - posy == 0: # If the Y vertex is the same, the movement is horizontal
+            if self.posx - posx > 0:
                 auxcoord = posx
-                while auxcoord <= self.coordinate.getX():
+                while auxcoord <= self.posx:
                     position.append(Coordinate(auxcoord, posy))
                     auxcoord = auxcoord + 1
-            elif self.coordinate.getX() - posx < 0:
+            elif self.posx - posx < 0:
                 auxcoord = posx
-                while auxcoord >= self.coordinate.getX():
+                while auxcoord >= self.posx:
                     position.append(Coordinate(auxcoord, posy))
                     auxcoord = auxcoord - 1
-        elif self.coordinate.getX() - posx == 0: # If the vertex X is the same, the movement is vertical
-            if self.coordinate.getY() - posy > 0:
+        elif self.posx - posx == 0: # If the vertex X is the same, the movement is vertical
+            if self.posy - posy > 0:
                 auxcoord = posy
-                while auxcoord <= self.coordinate.getY():
+                while auxcoord <= self.posy:
                     position.append(Coordinate(posx, auxcoord))
                     auxcoord = auxcoord + 1
-            elif self.coordinate.getY() - posy < 0:
+            elif self.posy - posy < 0:
                 auxcoord = posy
-                while auxcoord >= self.coordinate.getY():
+                while auxcoord >= self.posy:
                     position.append(Coordinate(posx, auxcoord))
                     auxcoord = auxcoord - 1
         return position
@@ -143,9 +148,9 @@ class Bishop(Piece):
     def canMove(self, posx, posy):
         positions = []
         # We initialize variables for Y and X vertex in order to check to which quadrant we are trying to move
-        cuadrantex = posx - self.coordinate.getX()
+        cuadrantex = posx - self.posx
         multiplicadorx = 1
-        cuadrantey = posy - self.coordinate.getY()
+        cuadrantey = posy - self.posy
         multiplicadory = 1
         if cuadrantey < 0:
             cuadrantey = -cuadrantey
@@ -157,7 +162,7 @@ class Bishop(Piece):
             i = 0
             # After we see which quadrant we are trying to move to, we use a loop to build the array of positions
             while cuadrantey >= 0:
-                positions.append(Coordinate(self.coordinate.getX() + (i * multiplicadorx), self.coordinate.getY() + (i * multiplicadory)))
+                positions.append(Coordinate(self.posx + (i * multiplicadorx), self.posy + (i * multiplicadory)))
                 cuadrantey = cuadrantey - 1
                 i = i + 1
         return positions
@@ -185,21 +190,21 @@ class Knight(Piece):
     def canMove(self, posx, posy):
         positions = []
         # As the Knight moves are very different, we have to check which one of the 8 possibilities we are trying to do
-        if (self.coordinate.getX() - posx == 2) and (self.coordinate.getY() - posy == 1):
+        if (self.posx - posx == 2) and (self.posy - posy == 1):
             positions.append(Coordinate(posx, posy))
-        elif (self.coordinate.getX() - posy == -2) and (self.coordinate.getY() - posy == -1):
+        elif (self.posx - posy == -2) and (self.posy - posy == -1):
             positions.append(Coordinate(posx, posy))
-        elif (self.coordinate.getX() - posx == 2) and (self.coordinate.getY() - posy == -1):
+        elif (self.posx - posx == 2) and (self.posy - posy == -1):
             positions.append(Coordinate(posx, posy))
-        elif (self.coordinate.getX() - posx == -2) and (self.coordinate.getY() - posy == 1):
+        elif (self.posx - posx == -2) and (self.posy - posy == 1):
             positions.append(Coordinate(posx, posy))
-        elif (self.coordinate.getY() - posy == 2) and (self.coordinate.getX() - posx == 1):
+        elif (self.posy - posy == 2) and (self.posx - posx == 1):
             positions.append(Coordinate(posx, posy))
-        elif (self.coordinate.getY() - posy == -2) and (self.coordinate.getX() - posx == -1):
+        elif (self.posy - posy == -2) and (self.posx - posx == -1):
             positions.append(Coordinate(posx, posy))
-        elif (self.coordinate.getY() - posy == -2) and (self.coordinate.getX() - posx == 1):
+        elif (self.posy - posy == -2) and (self.posx - posx == 1):
             positions.append(Coordinate(posx, posy))
-        elif (self.coordinate.getY() - posy == 2) and (self.coordinate.getX() - posx == -1):
+        elif (self.posy - posy == 2) and (self.posx - posx == -1):
             positions.append(Coordinate(posx, posy))
         return positions
 
@@ -226,9 +231,9 @@ class Queen(Piece):
     def canMove(self, posx, posy):
         # The queen is a fusion of the Rook and the Bishop, so we can merge the code to get the queens one
         position = []
-        cuadrantex = posx - self.coordinate.getX()
+        cuadrantex = posx - self.posx
         multiplicadorx = 1
-        cuadrantey = posy - self.coordinate.getY()
+        cuadrantey = posy - self.posy
         multiplicadory = 1
         if cuadrantey < 0:
             cuadrantey = -cuadrantey
@@ -239,30 +244,30 @@ class Queen(Piece):
         if cuadrantex == cuadrantey:
             i = 0
             while cuadrantey >= 0:
-                position.append(Coordinate(self.coordinate.getX() + (i * multiplicadorx), self.coordinate.getY() + (i * multiplicadory)))
+                position.append(Coordinate(self.posx + (i * multiplicadorx), self.posy + (i * multiplicadory)))
                 cuadrantey = cuadrantey - 1
                 i = i + 1
         auxcoord = 0
-        if self.coordinate.getY() - posy == 0:
-            if self.coordinate.getX() - posx > 0:
+        if self.posy - posy == 0:
+            if self.posx - posx > 0:
                 auxcoord = posx
-                while auxcoord <= self.coordinate.getX():
+                while auxcoord <= self.posx:
                     position.append(Coordinate(auxcoord, posy))
                     auxcoord = auxcoord + 1
-            elif self.coordinate.getX() - posx < 0:
+            elif self.posx - posx < 0:
                 auxcoord = posx
-                while auxcoord >= self.coordinate.getX():
+                while auxcoord >= self.posx:
                     position.append(Coordinate(auxcoord, posy))
                     auxcoord = auxcoord - 1
-        elif self.coordinate.getX() - posx == 0:
-            if self.coordinate.getY() - posy > 0:
+        elif self.posx - posx == 0:
+            if self.posy - posy > 0:
                 auxcoord = posy
-                while auxcoord <= self.coordinate.getY():
+                while auxcoord <= self.posy:
                     position.append(Coordinate(posx, auxcoord))
                     auxcoord = auxcoord + 1
-            elif self.coordinate.getY() - posy < 0:
+            elif self.posy - posy < 0:
                 auxcoord = posy
-                while auxcoord >= self.coordinate.getY():
+                while auxcoord >= self.posy:
                     position.append(Coordinate(posx, auxcoord))
                     auxcoord = auxcoord - 1
         return position
@@ -290,6 +295,6 @@ class King(Piece):
     def canMove(self, posx, posy):
         position = []
         # The king only has to check if it moved only one position
-        if -1 <= self.coordinate.getY() - posy <= 1 and -1 <= self.coordinate.getX() - posx <= 1:
+        if -1 <= self.posy - posy <= 1 and -1 <= self.posx - posx <= 1:
             position.append(Coordinate(posx, posy))
         return position
